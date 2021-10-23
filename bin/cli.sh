941e86e1f80f20487bf0d03b8879fdfcd7c67576
Livshitz <elya.livshitz@gmail.com>
2021-10-23 07:49:17 +0300

    * fix CLI paths

--- bin/cli.sh
+++ bin/cli.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-node build/Main.js $@
+# Get dir of executable
+dir=$( dirname $(realpath "$0") )/../
+# Navigate to the dir
+# cd $dir # commented out to keep releative paths (like output) intact
+# Execute the main script and pass the args
+node $dir/build/Main.js $@
